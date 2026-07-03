77b419548b95391b83dcf91686aeac1f16b23393
rrojek <r.rojek87@gmail.com>
2026-07-03 22:35:53 +0200

    add configurator policy

--- backend/modules/Configurator/Infrastructure/Providers/ConfiguratorServiceProvider.php
+++ backend/modules/Configurator/Infrastructure/Providers/ConfiguratorServiceProvider.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 
 namespace Modules\Configurator\Infrastructure\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Modules\Configurator\Domain\Contracts\AttributeCollectionRepositoryInterface;
 use Modules\Configurator\Domain\Contracts\AttributeRepositoryInterface;
 use Modules\Configurator\Domain\Contracts\AttributeValueRepositoryInterface;
 use Modules\Configurator\Domain\Contracts\DependencyRepositoryInterface;
 use Modules\Configurator\Domain\Contracts\StepRepositoryInterface;
+use Modules\Configurator\Domain\Models\Attribute;
+use Modules\Configurator\Domain\Models\AttributeCollection;
+use Modules\Configurator\Domain\Models\AttributeValue;
+use Modules\Configurator\Domain\Models\Dependency;
+use Modules\Configurator\Domain\Models\Step;
 use Modules\Configurator\Infrastructure\Persistence\Repositories\EloquentAttributeCollectionRepository;
 use Modules\Configurator\Infrastructure\Persistence\Repositories\EloquentAttributeRepository;
 use Modules\Configurator\Infrastructure\Persistence\Repositories\EloquentAttributeValueRepository;
 use Modules\Configurator\Infrastructure\Persistence\Repositories\EloquentDependencyRepository;
 use Modules\Configurator\Infrastructure\Persistence\Repositories\EloquentStepRepository;
+use Modules\Configurator\Presentation\Filament\Policies\ConfiguratorManagementPolicy;
 use Modules\Shared\Infrastructure\Providers\ModuleServiceProvider;
 
 final class ConfiguratorServiceProvider extends ModuleServiceProvider
@@ -30,5 +37,16 @@ final class ConfiguratorServiceProvider extends ModuleServiceProvider
         $this->app->bind(AttributeCollectionRepositoryInterface::class, EloquentAttributeCollectionRepository::class);
         $this->app->bind(AttributeValueRepositoryInterface::class, EloquentAttributeValueRepository::class);
         $this->app->bind(DependencyRepositoryInterface::class, EloquentDependencyRepository::class);
+    }
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        Gate::policy(Step::class, ConfiguratorManagementPolicy::class);
+        Gate::policy(Attribute::class, ConfiguratorManagementPolicy::class);
+        Gate::policy(AttributeCollection::class, ConfiguratorManagementPolicy::class);
+        Gate::policy(AttributeValue::class, ConfiguratorManagementPolicy::class);
+        Gate::policy(Dependency::class, ConfiguratorManagementPolicy::class);
     }
 }
